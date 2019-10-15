57746d3f2b6aa2fad33f756c2c43cd9a89420e28
mvaughan <magnus.vaughan@gmail.com>
2019-10-15 22:02:13 +0100

    Added Craze model/migration/controller

---
+++ database/migrations/2019_10_15_205406_create_crazes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrazesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crazes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->unsignedBigInteger('trend');
+            $table->foreign('trend')
+            ->references('id')->on('trends')
+            ->onDelete('cascade');
+            $table->unsignedBigInteger('location');
+            $table->foreign('location')
+            ->references('id')->on('locations')
+            ->onDelete('cascade');
+            $table->dateTimeTz('created_at');
+            $table->dateTimeTz('as_of');
+            $table->bigInteger('tweet_volume');
+            $table->boolean('promoted_content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crazes');
+    }
+}
