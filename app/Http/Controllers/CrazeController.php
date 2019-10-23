4c07b9c02a6a424aecae873290fd1230aaa56c88
mvaughan <magnus.vaughan@gmail.com>
2019-10-23 14:44:40 +0100

    Code to pull in worldwide images

--- app/Http/Controllers/CrazeController.php
+++ app/Http/Controllers/CrazeController.php
@@ -163,7 +163,8 @@ class CrazeController extends Controller
             ->where('location_images.location', '=', strval($country->location_id))
             ->get();
 
-            if(count($existing_country_images->all()) < 30) {
+            // if(count($existing_country_images->all()) < 30) 
+            if($country->location_id == 63 || $country->location_id == 64) {
 
                 $unsplash_access_key = env("UNSPLASH_ACCESS_KEY");
                 $unsplash_access_secret = env("UNSPLASH_ACCESS_SECRET");
