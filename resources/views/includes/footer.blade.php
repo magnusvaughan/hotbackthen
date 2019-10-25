eb4a53d49edb32bd19021fea1fcd0606e5d5e793
mvaughan <magnus.vaughan@gmail.com>
2019-10-25 18:52:23 +0100

    fixing CSS

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,3 @@
-<div>
-    <p>Archiving tweets from <a href="https://twitter.com">Twitter</a>. All images from <a href="https://unsplash.com">Unsplash</a></p>
-    © Made by <a target="__blank" href="https://twitter.com/magnusvaughan">Magnus</a>
-</div>
+
+<p>Archiving tweets from <a href="https://twitter.com">Twitter</a>. All images from <a href="https://unsplash.com">Unsplash</a></p>
+© Made by <a target="__blank" href="https://twitter.com/magnusvaughan">Magnus</a>
